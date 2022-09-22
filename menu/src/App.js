97386c461cf6004895ba0b4e839b64179ed66488
Chan405 <ayenyeinchanmoe2712@gmail.com>
2022-09-22 17:05:16 +0630

    react mini projects

--- menu/src/App.js
+++ menu/src/App.js
@@ -4,7 +4,36 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
-  return <h2>menu project setup</h2>;
+
+  const menu_categories =  new Set(items.map((item) => item.category));
+  const allcategories = ['all', ...menu_categories];
+  //console.log(allcategories);
+
+  const [menuitems, setMenuitems] = useState(items);
+  const [categories, setCategories] = useState(allcategories);
+
+  const filteritems = (category) => {
+    if(category === 'all'){
+      setMenuitems(items);
+      return;
+    }
+    const newItems = items.filter( (item) => item.category===category);
+    setMenuitems(newItems);
+  }
+
+  return(
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2> Our Menu </h2>
+          <div className="underline"> </div>
+        </div>
+
+        <Categories categories={categories} filteritems = {filteritems}/>
+        <Menu items={menuitems}/>
+      </section>
+    </main>
+    );
 }
 
 export default App;
